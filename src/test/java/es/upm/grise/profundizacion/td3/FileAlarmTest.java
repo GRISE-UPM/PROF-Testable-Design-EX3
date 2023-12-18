3e705a0f382d85f9f58cbe81c2669a85d5ca9bd5
Miguel Cid <cidespeso@gmail.com>
2023-12-18 20:28:26 +0100

    Done all tests

--- src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
@@ -24,50 +28,81 @@ import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 @ExtendWith(SystemStubsExtension.class)
 public class FileAlarmTest {
-	
+
 	@SystemStub
 	private EnvironmentVariables env = new EnvironmentVariables();
-	
+
 	FireAlarm fireAlarm;
-		
+
 	@BeforeEach
 	public void setUp() throws ConfigurationFileProblemException, DatabaseProblemException {
 		env.set("firealarm.location", System.getProperty("user.dir"));
 		fireAlarm = Mockito.spy(new FireAlarm());
 	}
-	
+
 	@Test
-	public void ConfigurationFileProblemIsThrownTest() {
+	public void ConfigurationFileProblemIsThrownTest() 
+	{
 		env.set("firealarm.location", "dummy");
 		assertThrows(ConfigurationFileProblemException.class, () -> new FireAlarm());
 	}
-		
+
 	@Test
-	public void DatabaseProblemExceptionIsThrown() 
-	{		
+	public void DatabaseProblemExceptionIsThrown() throws IOException{
+		Path filePath = Paths.get(System.getProperty("user.dir")+ "/resources/config.properties");
+		String originalContent = new String(Files.readAllBytes(filePath));
+		
+		FileWriter fw = new FileWriter(filePath.toString(), false);
+		fw.write("jdbc:sqlite:randomPath");
+		fw.close();
+		
+		assertThrows(DatabaseProblemException.class, () -> new FireAlarm());
+		
+		// Restore original db config
+		FileWriter fw2 = new FileWriter(filePath.toString(), false);
+		fw2.write(originalContent);
+		fw2.close();
 		
 	}
-	
+
 	@Test
 	public void SensorConnectionProblemExceptionIsThrown() 
-	{		
-		assertThrows(SensorConnectionProblemException.class, 
-				() -> new FireAlarm().getTemperature("dummy"));
+	{
+		assertThrows(SensorConnectionProblemException.class, () -> new FireAlarm().getTemperature("dummy"));
 	}
-	
+
 	@Test
-	public void isTemperatureTooLowTest() throws SensorConnectionProblemException, IncorrectDataException
-	{		
-		Mockito.doReturn(fireAlarm.MAX_TEMPERATURE - 1)
-			.when(fireAlarm).getTemperature(Mockito.anyString());
+	public void IncorrectDataExceptionIsThrown() throws IOException 
+	{
+		fireAlarm.mapper = Mockito.mock(ObjectMapper.class);
+
+		// No temperature key
+		JsonNode mockedResult = Mockito.mock(JsonNode.class);
+		Mockito.when(fireAlarm.mapper.readTree(Mockito.any(URL.class))).thenReturn(mockedResult);
+		Mockito.when(mockedResult.get("temperature")).thenReturn(null);
+		assertThrows(IncorrectDataException.class,
+				() -> fireAlarm.getTemperature("kitchen")); //kitchen is only key in .db?
+		
+		// Key value is not an integer
+		JsonNode temperature = Mockito.mock(JsonNode.class);
+		Mockito.when(mockedResult.get("temperature")).thenReturn(temperature);
+		Mockito.when(temperature.canConvertToInt()).thenReturn(false);
+		assertThrows(IncorrectDataException.class,
+				() -> fireAlarm.getTemperature("kitchen"));
+		
+	}
+
+	@Test
+	public void isTemperatureTooLowTest() throws SensorConnectionProblemException, IncorrectDataException 
+	{
+		Mockito.doReturn(fireAlarm.MAX_TEMPERATURE - 1).when(fireAlarm).getTemperature(Mockito.anyString());
 		assertFalse(fireAlarm.isTemperatureTooHigh());
 	}
-	
+
 	@Test
-	public void isTemperatureTooHighTest() throws SensorConnectionProblemException, IncorrectDataException
-	{		
-		Mockito.doReturn(fireAlarm.MAX_TEMPERATURE + 1)
-			.when(fireAlarm).getTemperature(Mockito.anyString());
+	public void isTemperatureTooHighTest() throws SensorConnectionProblemException, IncorrectDataException 
+	{
+		Mockito.doReturn(fireAlarm.MAX_TEMPERATURE + 1).when(fireAlarm).getTemperature(Mockito.anyString());
 		assertTrue(fireAlarm.isTemperatureTooHigh());
 	}
 
