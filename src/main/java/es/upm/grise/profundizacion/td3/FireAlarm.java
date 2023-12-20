d35cb7f53cdfc9763b2e3b9b546ebe9f838148e9
Luckvill <118052157+Luckvill@users.noreply.github.com>
2023-12-20 14:00:21 +0100

    Add files via upload

--- src/main/java/es/upm/grise/profundizacion/td3/FireAlarm.java
+++ src/main/java/es/upm/grise/profundizacion/td3/FireAlarm.java
@@ -1,5 +1,6 @@
 package es.upm.grise.profundizacion.td3;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -15,14 +16,18 @@ import java.sql.ResultSet;
 import java.net.URL;
 
 public class FireAlarm {
-	
+
 	// Sensors are stored in a hash map for easier access
 	private HashMap<String, String> sensors = new HashMap<String, String>();
-	
+
 	// Constructor: read the sensors from the database and store them
 	// in the hash map
+	private Connection connection;
+	private String dblocation;
+	private ObjectMapper mapper=new ObjectMapper();
+
 	public FireAlarm() throws ConfigurationFileProblemException, DatabaseProblemException {
-		
+
 		// Read the property file to find out the database location
 		// As the executable can be located anywhere, so we store the
 		// app directory in a environment variable (without the slash
@@ -31,24 +36,28 @@ public class FireAlarm {
 		String appLocation = System.getenv("firealarm.location");
 
 		try {
-			
-			configProperties.load(new FileInputStream(appLocation + "/resources/config.properties"));
-			
+
+			if(File.separator.equals("\\"))
+				configProperties.load(new FileInputStream(appLocation + File.separator + File.separator + "resources" + File.separator + File.separator + "config.properties"));
+			else
+				configProperties.load(new FileInputStream(appLocation + File.separator + "resources" + File.separator + "config.properties"));
+
+
 		} catch (Exception e) {
-			
+
 			throw new ConfigurationFileProblemException();
-			
+
 		}
-		  
+
 		// Then we obtain the database location
-		String dblocation = configProperties.getProperty("dblocation");
-		
+		dblocation = configProperties.getProperty("dblocation");
+
 		// Now we store the sensors' data in the sensors variable
 		// It takes several steps determined by the SQL API
 		try {
-			
+
 			// Create DB connection
-			Connection connection = DriverManager.getConnection(dblocation);
+			connection = DriverManager.getConnection(dblocation);
 
 			// Read from the sensors table
 			String query = "SELECT * FROM sensors";
@@ -57,30 +66,55 @@ public class FireAlarm {
 
 			// Iterate until we get all sensors' data
 			while (resultSet.next()) {
-				
+
 				String room = resultSet.getString("room");
 				String endpoint = resultSet.getString("endpoint");
 				sensors.put(room, endpoint);
-				
+
 			}
 
 			// Close the connection
 			connection.close();
 
 		} catch (Exception e) {
-			
-			throw new DatabaseProblemException(); 
-			
+
+			throw new DatabaseProblemException();
+
 		}
 
 	}
 
+	// Método setter para ObjectMapper
+	public void setMap(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
+
+	public void consult(String consulta) throws DatabaseProblemException {
+		try {
+
+			// Create DB connection
+			connection = DriverManager.getConnection(dblocation);
+
+			// Read from the sensors table
+			String query = consulta;
+			Statement statement = connection.createStatement();
+			statement.executeQuery(query);
+
+			// Close the connection
+			connection.close();
+
+		} catch (Exception e) {
+
+			throw new DatabaseProblemException();
+
+		}
+	}
+
 	// Read the temperature from a sensor
-	private int getTemperature(String room) throws SensorConnectionProblemException, IncorrectDataException {
+	public int getTemperature(String room) throws SensorConnectionProblemException, IncorrectDataException {
 
 		String endpoint = sensors.get(room);
 		URL url;
-		ObjectMapper mapper = new ObjectMapper();
 		JsonNode result;
 
 		// Using the Jackson library we can get JSON directly from an
@@ -91,43 +125,43 @@ public class FireAlarm {
 		} catch (Exception e) {
 			throw new SensorConnectionProblemException();
 		}
-		
+
 		// If no JSON data is returned, we raise an exception
 		if(result == null)
 			throw new IncorrectDataException();
-		
+
 		// The sensor returns an JSON object with a single key/value
 		// pair named "temperature".
 		result = result.get("temperature");
-		
+
 		// The key "temperature" may not exist
 		if(result == null)
 			throw new IncorrectDataException();
-		
+
 		// If the value is not integer, we raise the same error
 		if(!result.canConvertToInt())
 			throw new IncorrectDataException();
 
 		// When everything is correct, we return the temperature as an Int
 		return result.asInt();
-		
+
 	}
 
 
 	public boolean isTemperatureTooHigh() throws SensorConnectionProblemException, IncorrectDataException {
-		
+
 		final int MAX_TEMPERATURE = 80;
-		
-		// If any temperature exceeds MAX_TEMPERATURE, then the 
+
+		// If any temperature exceeds MAX_TEMPERATURE, then the
 		// temperature is too high
 		for(Entry<String, String> sensor : sensors.entrySet()) {
-			
+
 			if(getTemperature(sensor.getKey()) > MAX_TEMPERATURE)
-					return true;
+				return true;
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 }
