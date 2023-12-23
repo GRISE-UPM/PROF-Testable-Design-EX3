7414ebc3b1aed67c27efc24c193ed23187470814
LuisBMI <luisbmi55@gmail.com>
2023-12-23 18:00:36 +0100

    Add test dependencies and first test

--- src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
@@ -1,22 +1,46 @@
 package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
+
+@ExtendWith(SystemStubsExtension.class)
 public class FileAlarmTest {
 	
 	FireAlarm fireAlarm;
+
+	@SystemStub
+	private EnvironmentVariables envVariables = new EnvironmentVariables(
+		"firealarm.location", System.getProperty("user.dir")
+	);
+	
 	
 	@BeforeEach
-	public void setUp() throws ConfigurationFileProblemException, DatabaseProblemException {
+	public void setUp() throws ConfigurationFileProblemException, DatabaseProblemException 
+	{
 		fireAlarm = new FireAlarm();
 	}
 	
+	/*
+	 * When the config.properties file cannot be located, 
+	 * the FireAlarm class throws a ConfigurationFileProblemException.
+	 */
 	@Test
-	public void test() throws SensorConnectionProblemException, IncorrectDataException {
-		assertFalse(fireAlarm.isTemperatureTooHigh());
+	public void testConfigurationFileNotFound() 
+	{
+		envVariables.set("firealarm.location", "non-existing-file");
+		assertThrows(ConfigurationFileProblemException.class, () -> {
+			fireAlarm = new FireAlarm();
+		});
 	}
+
+	
 
 }
