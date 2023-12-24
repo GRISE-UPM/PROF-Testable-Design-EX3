2d80f7567cf690413d3dfd19120668ad7f17b481
LuisBMI <luisbmi55@gmail.com>
2023-12-24 17:17:43 +0100

    Refactor FireAlarm class and add third test

--- src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
@@ -2,6 +2,7 @@ package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,7 @@ public class FileAlarmTest {
 	}
 	
 	/*
+	 * a)
 	 * When the config.properties file cannot be located, 
 	 * the FireAlarm class throws a ConfigurationFileProblemException.
 	 */
@@ -44,6 +46,7 @@ public class FileAlarmTest {
 	}
 
 	/*
+	 * b)
 	 * Any database error, e.g. connection problem or query error, 
 	 * implies the launching of a DatabaseProblemException.
 	 * For this test, query was added as a parameter to the constructor.
@@ -56,6 +59,19 @@ public class FileAlarmTest {
 		query = "SELECT * FROM NON_EXISTING_TABLE";
 		assertThrows(DatabaseProblemException.class, () -> {
 			fireAlarm = new FireAlarm(query);
+		});
+	}
+
+	/*
+	 * c)
+	 * When the REST endpoint is not usable, 
+	 * the application throws a SensorConnectionProblemException.
+	 * Changed the getTemperature method to protected.
+	 */
+	@Test
+	public void testSensorConnectionProblemException() throws SensorConnectionProblemException, IncorrectDataException {
+		assertThrows(SensorConnectionProblemException.class, () -> {
+			fireAlarm.getTemperature("potato-room");
 		});
 	}
 
