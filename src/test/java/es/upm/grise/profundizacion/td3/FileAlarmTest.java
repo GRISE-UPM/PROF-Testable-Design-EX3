8853e1cd1e07a1d6e2409da4afbdcc44898a051e
LuisBMI <luisbmi55@gmail.com>
2023-12-24 23:19:08 +0100

    added IncorrectDataException tests

--- src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
+++ src/test/java/es/upm/grise/profundizacion/td3/FileAlarmTest.java
@@ -2,11 +2,20 @@ package es.upm.grise.profundizacion.td3;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.net.URL;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.TextNode;
 
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
@@ -25,8 +34,7 @@ public class FileAlarmTest {
 	
 	
 	@BeforeEach
-	public void setUp() throws ConfigurationFileProblemException, DatabaseProblemException 
-	{
+	public void setUp() throws ConfigurationFileProblemException, DatabaseProblemException {
 		query = "SELECT * FROM SENSORS";
 		fireAlarm = new FireAlarm(query);
 	}
@@ -37,9 +45,9 @@ public class FileAlarmTest {
 	 * the FireAlarm class throws a ConfigurationFileProblemException.
 	 */
 	@Test
-	public void testConfigurationFileNotFound() 
-	{
+	public void configurationFileNotFound() {
 		envVariables.set("firealarm.location", "non-existing-file");
+
 		assertThrows(ConfigurationFileProblemException.class, () -> {
 			fireAlarm = new FireAlarm(query);
 		});
@@ -54,9 +62,9 @@ public class FileAlarmTest {
 	 * making FireAlarm more flexible and testable.
 	 */
 	@Test
-	public void testDatabaseError() 
-	{
+	public void databaseError() {
 		query = "SELECT * FROM NON_EXISTING_TABLE";
+
 		assertThrows(DatabaseProblemException.class, () -> {
 			fireAlarm = new FireAlarm(query);
 		});
@@ -69,7 +77,7 @@ public class FileAlarmTest {
 	 * Changed the getTemperature method to protected.
 	 */
 	@Test
-	public void testSensorConnectionProblemException() throws SensorConnectionProblemException, IncorrectDataException {
+	public void sensorConnectionProblemException() throws SensorConnectionProblemException, IncorrectDataException {
 		assertThrows(SensorConnectionProblemException.class, () -> {
 			fireAlarm.getTemperature("potato-room");
 		});
@@ -80,11 +88,65 @@ public class FileAlarmTest {
 	 * Changed sensors to protected.
 	 */
 	@Test
-	public void testIncorrectDataException() throws SensorConnectionProblemException, IncorrectDataException {
+	public void incorrectDataException() throws SensorConnectionProblemException, IncorrectDataException {
 		fireAlarm.sensors.put("potato-room", "http://steam.com");
+
 		assertThrows(SensorConnectionProblemException.class, () -> {
 			fireAlarm.getTemperature("potato-room");
 		});
 	}
+
+	/*
+	 * d)
+	 * If no JSON data is returned, we raise an IncorrectDataException
+	 * the application throws an IncorrectDataException.
+	 * I had to change the objectMapper to protected.
+	 */
+	@Test
+	public void incorrectDataExceptionNoTemperature() throws IncorrectDataException, IOException {
+		ObjectMapper mapperMock = mock(ObjectMapper.class);
+		fireAlarm.mapper = mapperMock;
+
+		when(mapperMock.readTree("")).thenReturn(null);
+		assertThrows(IncorrectDataException.class, () -> 
+		{
+			fireAlarm.getTemperature("kitchen");
+		});
+	}
+
+	/*
+	 * If the returned JSON object does not contain the key "temperature", 
+	 * the application throws an IncorrectDataException.
+	 */
+	@Test
+	public void incorrectDataExceptionNoTemperatureKey() throws IncorrectDataException, IOException {
+		ObjectMapper mapperMock = mock(ObjectMapper.class);
+		fireAlarm.mapper = mapperMock;
+		JsonNode jsonNodeMock = mock(JsonNode.class);
+
+		when(jsonNodeMock.get("temperature")).thenReturn(null);
+		when(mapperMock.readTree(any(URL.class))).thenReturn(jsonNodeMock);
+		assertThrows(IncorrectDataException.class, () -> {
+			fireAlarm.getTemperature("kitchen");
+		});
+	}
+
+	/*
+	 * If the returned value is not integer, 
+	 * the application throws an IncorrectDataException.
+	 */
+	@Test
+	public void incorrectDataExceptionTemperatureNotInteger() throws IncorrectDataException, IOException {
+		ObjectMapper mapperMock = mock(ObjectMapper.class);
+		fireAlarm.mapper = mapperMock;
+		JsonNode jsonNodeMock = mock(JsonNode.class);
+
+		when(jsonNodeMock.get("temperature")).thenReturn(new TextNode("not integer"));
+		when(mapperMock.readTree(any(URL.class))).thenReturn(jsonNodeMock);
+		assertThrows(IncorrectDataException.class, () -> {
+			fireAlarm.getTemperature("kitchen");
+		});
+	}
+
 
 }
