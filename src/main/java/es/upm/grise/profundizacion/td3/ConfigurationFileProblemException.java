b50eb9c82bf61edb493df7e30ac0433499c3f024
rubenixter <124615627+rubenixter@users.noreply.github.com>
2024-01-30 12:35:56 +0100

    Delete src directory

--- src/main/java/es/upm/grise/profundizacion/td3/ConfigurationFileProblemException.java
+++
@@ -1,5 +0,0 @@
-package es.upm.grise.profundizacion.td3;
-
-public class ConfigurationFileProblemException extends Exception {
-
-}
--- src/main/java/es/upm/grise/profundizacion/td3/IncorrectDataException.java
+++
@@ -1,5 +0,0 @@
-package es.upm.grise.profundizacion.td3;
-
-public class IncorrectDataException extends Exception {
-
-}
